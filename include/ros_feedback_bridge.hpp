6b7b95558c54d836e281af45d1503f81a714b548
Indraneel <neilpatilcoldplay@gmail.com>
2022-02-11 15:18:37 -0500

    Revert "publish odometry, added mutex locks"
    This reverts commit 6f53c006e309ebad507ab328147c84ca029042ef.

--- include/ros_feedback_bridge.hpp
+++ include/ros_feedback_bridge.hpp
@@ -13,7 +13,6 @@
 #include <angles/angles.h>
 #include "odom_node.hpp"
 #include <nav_msgs/Odometry.h>
-#include <mutex>
 
 // TODO
 // Cross check variable reshuffling by neel
@@ -68,7 +67,6 @@ public:
         imu_publisher_.publish(imu_msg);
 
         // encoder callback
-        std::lock_guard<std::mutex> guard(mtx);
         pos_left = feedback.count_data().left();
         pos_right = feedback.count_data().right();
         ROS_INFO("Left ticks: %d", pos_left);
@@ -102,7 +100,7 @@ public:
     void runOdometry()
     {
         //ROS_INFO("Spinning odometry with delay %d ms",odom_delay_ms);
-        
+
         // Set the data fields of the odometry message
         odomNew.header.frame_id = "odom";
         odomNew.pose.pose.position.z = 0;
@@ -120,16 +118,15 @@ public:
 
         while(node_alive_)
         {
-            std::lock_guard<std::mutex> guard(mtx);
             update_odom(odom_data_pub);
             publish_quat(odom_data_pub_quat);
+
             std::this_thread::sleep_for(std::chrono::milliseconds(odom_delay_ms));   
         }
     }
 
 
 private:
-    std::mutex mtx;
     ros::NodeHandle nh_;
     ros::Publisher imu_publisher_;
     ros::Publisher odom_data_pub;
